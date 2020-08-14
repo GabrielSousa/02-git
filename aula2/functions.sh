fcbeb1c64bc4cf7753fabdc4e8c583f43fe81007
Gabriel Sousa <gabrielgomesdesousa.ggs@gmail.com>
2020-08-14 16:22:04 -0300

    Download e descompactação seletivos.
    Closes: #1

--- aula2/functions.sh
+++ aula2/functions.sh
@@ -3,10 +3,25 @@
 
 function download_datasets {
     if [[ -d "$1" ]]; then
-        echo "Baixando arquivo de 2006"
-        wget -q 'https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/HG7NV7/EPIFFT' -O "${1}/2006.tar.bz2"
-        echo "Baixando arquivo de 2007"
-        wget -q 'https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/HG7NV7/2BHLWK' -O "${1}/2007.tar.bz2"
+        if [[ ! -f "$1/2006.csv" ]]; then
+            if [[ ! -f "$1/2006.csv.bz2" ]]; then
+                echo "Baixando arquivo de 2006"
+                wget -q 'https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/HG7NV7/EPIFFT' -O "${1}/2006.csv.bz2"
+            fi
+            echo "Descompactando arquivo de 2006"
+            bzip2 --decompress "$1/2006.csv.bz2"
+        fi
+
+
+        if [[ ! -f "$1/2007.csv" ]]; then
+            if [[ ! -f "$1/2007.csv.bz2" ]]; then
+                echo "Baixando arquivo de 2007"
+                wget -q 'https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/HG7NV7/2BHLWK' -O "${1}/2007.csv.bz2"
+            fi
+            echo "Descompactando arquivo de 2007"
+            bzip2 --decompress "$1/2007.csv.bz2"
+        fi
+
         exit 0
     else
         echo "Destino do download não existe"
---
+++ aula2/test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir -p data
+
+./flight-delays.sh -d data
