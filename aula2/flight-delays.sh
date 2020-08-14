3598929487ac0a84512ee37a7b00998983564f70
tgopedrosa <tgopedrosa@gmail.com>
2020-08-14 17:12:41 -0300

    feature para extrair número de atrasos, recebendo como parâmetro, o ano de referência

--- aula2/flight-delays.sh
+++ aula2/flight-delays.sh
@@ -2,15 +2,22 @@
 
 source functions.sh
 
-while getopts ":dt" opt; do
+while getopts ":dy" opt; do
     case ${opt} in
         d ) # process option h
             shift # Removes de First Argument from the queue
             download_datasets $1
         ;;
-        t ) # process option t
+        y ) # process option n
+            shift
+            if [ -e "data/$1.csv" ] ; then 
+                value=$(awk -F , '{ if (toupper($15) > 0)  print }' data/$1.csv | wc -l) 
+                echo "Em $1 tiveram $value voos atrasados"
+            else 
+                echo "Arquivo não existe"
+            fi
         ;;
-        \? ) echo "Usage: flight-delays.sh [-d] [-t]"
+        \? ) echo "Usage: flight-delays.sh [-d] [-y]"
         ;;
   esac
 done
