583d7526fc391f15f51d17e51fb33b2e41b53da5
tgopedrosa <tgopedrosa@gmail.com>
2020-08-17 15:43:09 -0300

    Feature para listar o nome dos aeroportos com atraso

--- aula2/flight-delays.sh
+++ aula2/flight-delays.sh
@@ -2,7 +2,7 @@
 
 source functions.sh
 
-while getopts ":dy" opt; do
+while getopts ":dya" opt; do
     case ${opt} in
         d ) # process option h
             shift # Removes de First Argument from the queue
@@ -21,6 +21,12 @@ while getopts ":dy" opt; do
             fi
         
             echo "Em $1 tiveram $value voos atrasados"
+        ;;
+        a ) #process option a
+            shift
+            airport_ids=$(awk -F , '{print $17}' data/atrasados_$1.csv | sort | uniq )
+            awk -F',' -v ai="${airport_ids}" 'BEGIN{split(ai, codes, " "); for (c in codes) dict[codes[c]]=""} {gsub(/"/,"",$1)} $1 in dict {print $2}' data/airports.csv 
+            #awk -F , '$1 ~ /ABI/ {print $2}' data/airports.csv 
         ;;
         \? ) echo "Usage: flight-delays.sh [-d] [-y]"
         ;;
